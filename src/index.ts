e066c8faa77f9c8cfb5f741a2b2c8a6df2a66de9
Apollo Daniel <developer.apollo.mail@gmail.com>
2024-10-29 10:54:26 -0300

    Added cors policy

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,9 @@ const app = express();
 app.use(express.json());
 
 app.use(
-	// 	cors({
-	// 		origin: 'apollodaniel.codemagic.app',
-	// 	}),
-	cors(),
+	cors({
+		origin: 'https://portfolio-amber-tau-58.vercel.app',
+	}),
 );
 
 app.use(mainRouter);
